6c46652c4372fbae9bc4e0a743ae140befe1e51f
Rishu Kumar Pandey <richrishu69@gmail.com>
2026-08-28 14:32:31 +0530

    0079-WORD-SEARCH Stats: Time: 128 ms (87.17%), Space: 43.1 MB (28.18%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (backtrack(board, word, r, c, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean backtrack(char[][] board, String word, int r, int c, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length
+            || board[r][c] != word.charAt(index)) {
+            return false;
+        }
+        char temp = board[r][c];
+        board[r][c] = '#';
+
+        // index++ mt kr lena ð kyunki index change nhi krna bas usko pass krna h +1 krke
+        boolean found = backtrack(board, word, r + 1, c, index + 1)
+            || backtrack(board, word, r - 1, c, index + 1)
+            || backtrack(board, word, r, c + 1, index + 1)
+            || backtrack(board, word, r, c - 1, index + 1);
+
+        board[r][c] = temp;
+        return found;
+    }
+}
