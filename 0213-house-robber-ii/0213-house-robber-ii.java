0fca303612fd38490034ae6719fd9ed09409f31b
Rishu Kumar Pandey <richrishu69@gmail.com>
2026-09-07 00:54:27 +0530

    0213-HOUSE-ROBBER-II Stats: Time: 0 ms (100%), Space: 43 MB (8.39%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,11 +1,12 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1)
+        if (n == 1) {
             return nums[0];
-        if (n == 2)
+        }
+        if (n == 2) {
             return Math.max(nums[0], nums[1]);
-
+        }
         // Case 1: House 0 to n-2
         int ans1 = help(nums, 0, n - 2);
 
@@ -15,7 +16,7 @@ class Solution {
         return Math.max(ans1, ans2);
     }
 
-    // Isme wahi tera House Robber I ka O(1) space logic aayega!
+    // Isme wahi House Robber I ka O(1) space logic aayega!
     private int help(int[] nums, int start, int end) {
         int one = nums[start];
         int two = Math.max(one, nums[start + 1]);
